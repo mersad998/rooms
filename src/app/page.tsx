6975102ac2fd38176269fb6e8bf01e2fee97b473
mersad <mersad_998@yahoo.com>
2024-07-18 14:19:36 +0330

    feat: create a responsive card for display info

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 'use client';
 
-import { Typography } from '@mui/material';
+import ApartmentCard from '@/components/apartmentCard';
+import ResponsiveGrid from '@/components/ResponsiveGrid';
+
 import { FC } from 'react';
 
 const HomePage: FC = () => {
   return (
     <div>
-      <Typography>This page is going to be Home Page</Typography>
+      <ResponsiveGrid list={[1, 2, 3, 4, 5, 6, 7]} component={<ApartmentCard />} />
     </div>
   );
 };
